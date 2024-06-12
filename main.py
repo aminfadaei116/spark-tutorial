0a0bb1f71a23d01ebb55d3cae38c561350450c6c
Amin Fadaeinejad <51967292+aminfadaei116@users.noreply.github.com>
2024-06-12 00:54:44 -0400

    Update main.py and requirement.txt

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pyspark
+import pandas as pd
+from pyspark.sql import SparkSession
 
 
 def main():
-    print("hi")
+    data = pd.read_csv("./datasets/pizza_sales/order_details.csv")
+    print(data)
+
 
 
 if __name__ == "__main__":
