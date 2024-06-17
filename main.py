4ef47db18d1c7d7940dbad539e2b33da4c66799b
Amin Fadaeinejad <51967292+aminfadaei116@users.noreply.github.com>
2024-06-16 23:23:50 -0400

    Add new dataset which has missing values, and modified the notebook scripts

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from pyspark.sql import SparkSession
 
 
 def main():
-    data = pd.read_csv("./datasets/pizza_sales/order_details.csv")
+    pizza_dataset_path = "./datasets/pizza_sales/order_details.v2.csv"
+    spark = SparkSession.builder.appName('Learning Spark').getOrCreate()
+    data = spark.read.option('header', 'true').csv(pizza_dataset_path, inferSchema=True)
+
     print(data)
-    spec = SparkSession.builder.appName('Learning Spark').getOrCreate()
-    spec.stop()
-    print(spec)
+    new_data = data.na.fill('Missing values')
+    print(data)
 
 
 
